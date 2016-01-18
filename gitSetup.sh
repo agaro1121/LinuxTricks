70fec4c40939f33a2c624e1a946eca1e4528e6d2
Anthony Garo <agaro1121@gmail.com>
2016-01-17 20:03:28 -0500

    Update gitSetup.sh

--- gitSetup.sh
+++ gitSetup.sh
@@ -3,6 +3,11 @@
 curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
 curl -L https://raw.githubusercontent.com/agaro1121/LinuxTricks/master/.git-prompt.sh > ~/.git-prompt.sh
 
+sudo apt-get update && sudo apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+cd /dev/lib/git
+make prefix=/usr/local all
+sudo make prefix=/usr/local install
+
 git config --global color.ui true
 git config --global core.editor nano
 git config --global --bool bash.showDirtyState true
